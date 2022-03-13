623e5665cb25d406302573cd6b21863218aea226
ShinjaeKwon <sjk6437@gmail.com>
2022-03-13 22:11:00 +0900

    feat : setup shop project

---
+++ src/main/java/com/sjk/shop/SjkShopApplication.java
@@ -0,0 +1,13 @@
+package com.sjk.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjkShopApplication.class, args);
+	}
+
+}
